20ca8c0b1e3c6e03d569696261d353e80d2542f2
Piotr Leoszewski <p.leoszewski@gmail.com>
2018-03-20 20:22:36 +0100

    Add bigSlide

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.menu-link').bigSlide({
+        menuWidth:'18em',
+        push:'(.mypush)',
+        speed:'450',
+        side: right,
+        easyClose: true
+    });
+});
